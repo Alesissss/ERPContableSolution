28cd870f0db70b745cae93858bba2c0202cbd288
Alesisss <gfake040305@gmail.com>
2025-12-10 12:18:54 -0500

    css y js

---
+++ ERPContable/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ERPContable.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ERPContable.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Propiedad DbSet por cada entidad/tabla
+        public DbSet<Empresa> Empresas { get; set; }
+    }
+}
