ee255c5dbcbf27636037944a00eef8c7ba7f7a0e
Alesisss <gfake040305@gmail.com>
2025-12-10 10:39:07 -0500

    Agregar archivos de proyecto.

---
+++ ERPContable/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ERPContable.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
